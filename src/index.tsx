647e7fb40fbd19f32dc945901e3594d0b24c021d
mattmonjan-tech <matt.monjan@infusedu.com>
2025-11-24 12:57:15 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// @ts-ignore
 import App from './App';
-import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
