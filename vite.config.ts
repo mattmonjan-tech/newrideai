9f3799731cbc5186a09abbba1dfea8b8fcfb552f
mattmonjan-tech <matt.monjan@infusedu.com>
2025-12-04 12:48:29 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -16,7 +16,8 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        '@': '/src',
+        // Correctly resolve '@' to the project root 'src' folder
+        '@': path.resolve(__dirname, './src'),
       },
     },
   }
